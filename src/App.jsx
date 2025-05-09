ca0575afef5f973d6ac2cd27c1d25a3a0f9d739c
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-05-09 15:21:19 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+
+function App() {
+
+
+  return (
+    <>
+      <header>
+        <div className="navbar bg-primary-subtle mb-4">
+          <div className="container-fluid">
+            <h1>React Blog Form</h1>
+          </div>
+        </div>
+      </header>
+
+      <main>
+        <div className="container">
+          <ul className="list-group">
+            <li className='list-group-item'>prova</li>
+            <li className='list-group-item'>prova</li>
+            <li className='list-group-item'>prova</li>
+            <li className='list-group-item'>prova</li>
+            <li className='list-group-item'>prova</li>
+          </ul>
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default App
