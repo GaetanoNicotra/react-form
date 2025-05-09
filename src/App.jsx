d1581e4d223ddc551d87d6b212bdec1daaf93512
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-05-09 17:30:01 +0200

    [FIX] aggiunta correttamente in lista la nuova bike

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,29 @@
 import { useState } from 'react'
 
+// definisco l'array con gli elementi della mia lista
+const roadBike = [
+  { id: 1, name: 'Pinarello' },
+  { id: 2, name: 'Bianchi' },
+  { id: 3, name: 'Santa Cruz' },
+  { id: 4, name: 'Specialized' },
+  { id: 5, name: 'Cannodale' },
+  { id: 6, name: 'Trek' },
+  { id: 7, name: 'Colnago' },
+  { id: 8, name: 'Scott' }
+];
+
 function App() {
+  const [newBike, setNewBike] = useState("");
+  const [bike, setBike] = useState(roadBike);
 
-  // definisco l'array con gli elementi della mia lista
-  const roadBike = [
-    {
-      id: 1,
-      name: 'Pinarello',
-
-    },
-    {
-      id: 2,
-      name: 'Bianchi',
-
-    },
-    {
-      id: 3,
-      name: 'Santa Cruz',
-    },
-    {
-      id: 4,
-      name: 'Specialized',
-    },
-    {
-      id: 5,
-      name: 'Cannodale',
-
-    },
-    {
-      id: 6,
-      name: 'Trek',
-
-    },
-    {
-      id: 7,
-      name: 'Colnago',
-
-    },
-    {
-      id: 8,
-      name: 'Scott',
-
-    }
-  ];
+  const addBike = (e) => {
+    e.preventDefault();
+    const newBikeId = ;
+    const newBikeObject = { id: newBikeId, name: newBike }
+    console.log(newBikeObject)
+    setBike([...bike, newBikeObject])
+    setNewBike("")
+  };
 
   return (
     <>
@@ -58,14 +39,17 @@ function App() {
         <div className="container">
           {/* elenco degli elementi della lista */}
           <ul className="list-group">
-            <li class="list-group-item active" aria-current="true"> Bike list</li>
-            {roadBike.map(bike => (
-              <li className='list-group-item' key={bike.id} >{bike.name} </li>
+            <li className="list-group-item active" aria-current="true"> Bike list</li>
+            {bike.map(bikeName => (
+              <li className='list-group-item' key={bikeName.id} >{bikeName.name} </li>
             ))}
           </ul>
-          <form>
+          <form onSubmit={addBike}>
             <div className="input-group mt-3">
-              <input type="text" className='form-controll px-4' placeholder='Inserisci una nuova bici' />
+              <input value={newBike} onChange={(e) => {
+                setNewBike(e.target.value)
+              }} type="text" className='px-4' placeholder='Inserisci una nuova bici' />
+              <button className='btn btn-primary ms-3'> Aggiungi</button>
             </div>
           </form>
         </div>
@@ -74,4 +58,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
